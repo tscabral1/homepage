b516d2f7e50e256c0722f7d678ac87f2123ffca3
tscabral1 <44780117+tscabral1@users.noreply.github.com>
2025-01-01 10:57:08 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       {/* General Favorites */}
       <div>      
         <Favorite
-          url="https://docs.google.com/spreadsheets/d/1ol957s5LebVM3ZyMDb7c93Wde5WRxpZ6_lIRJ6p_l-E/edit#gid=656989373"
+          url="https://docs.google.com/spreadsheets/d/1G2kIeOkdKvTIvlwt4yLxlyheYwPx8e1WMuiNJ7Ge5C8/edit?gid=656989373#gid=656989373"
           title="Finanças"
           icon={<FaFileExcel />}
           color="text-[green]"
